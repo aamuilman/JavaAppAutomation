b221763b0a45e536940ad6c42fb130370aa41023
aamuilman <aamuilman@gmail.com>
2023-08-29 15:14:14 +0300

    Search Text

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,17 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -37,6 +45,44 @@ public class FirstTest {
 
     public void firstTest()
     {
-        System.out.println("Firts test run f");
+
+
+       // WebElement elementToInitSearch = driver.findElementByXPath("//*[contains(@text,'Search Wikipedia')]");
+        // elementToInitSearch.click();
+
+
+        assertElementHasText(
+                "//*[contains(@text,'Search Wikipedia')]",
+                "Cannot find expectedText",
+                "Search Wikipedia",
+                5
+        );
+        // driver.findElementByXPath("//*[contains(@text,'Search…')]");
+
+        // System.out.println("Firts test run f");
+
+
     }
+
+    private WebElement waitForElementPresentByXpath(String xpath, String errorMessage, long timeoutInSeconds)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(errorMessage + "\n");
+
+        By by = By.xpath(xpath);
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+    private WebElement waitforElementPresentByXpath(String xpath, String errorMessage) {
+        return waitForElementPresentByXpath(xpath, errorMessage,5 );
+    }
+
+    private WebElement assertElementHasText (String xpath, String errorMessage, String expectedText,long timeInSeconds )
+    {
+        WebElement element1 = waitForElementPresentByXpath(xpath,errorMessage,timeInSeconds);
+        Assert.assertEquals(expectedText,element1.getText());
+        return element1;
+    }
+
 }
