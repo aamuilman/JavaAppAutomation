50dd503b2693e5ac6c45c3cfaf9526c2537a5cb0
aamuilman <aamuilman@gmail.com>
2023-09-04 17:31:00 +0300

    Find and close

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,87 +1,97 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.net.URL;
-
 public class FirstTest {
 
     private AppiumDriver driver;
 
     @Before
-    public void setUp() throws Exception
-    {
+    public void setUp() throws Exception {
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        capabilities.setCapability("platformName","Android");
-        capabilities.setCapability("deviceName","and80");
-        capabilities.setCapability("platformVersion","8.0");
-        capabilities.setCapability("automationName","Appium");
-        capabilities.setCapability("appPackage","org.wikipedia");
-        capabilities.setCapability("appActivity",".main.MainActivity");
-        capabilities.setCapability("app","/Volumes/KINGSTON/JavaAppiumAutomation/JavaAppAutomation/apks/org.wikipedia.apk");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "and80");
+        capabilities.setCapability("platformVersion", "8.0");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "/Volumes/KINGSTON/JavaAppiumAutomation/JavaAppAutomation/apks/org.wikipedia.apk");
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 
     }
+
     @After
-    public void tearDown()
-    {
+    public void tearDown() {
         driver.quit();
     }
 
-
     @Test
+    public void firstTest() {
 
-    public void firstTest()
-    {
-
-
-       // WebElement elementToInitSearch = driver.findElementByXPath("//*[contains(@text,'Search Wikipedia')]");
-        // elementToInitSearch.click();
-
-
-        assertElementHasText(
-                "//*[contains(@text,'Search Wikipedia')]",
-                "Cannot find expectedText",
-                "Search Wikipedia",
+        waitAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find Search Wikipedia",
                 5
         );
-        // driver.findElementByXPath("//*[contains(@text,'Search…')]");
+        waitAndSendKeys(
+                By.xpath("//*[contains(@text, 'Search…')]"),
+                "Java",
+                "Cannot find text",
+                5
+        );
+        waitForElement(
+                By.id("org.wikipedia:id/page_list_item_title"),
+                "No search results",
+                5);
 
-        // System.out.println("Firts test run f");
+        waitAndClick(
+                By.id("org.wikipedia:id/search_close_btn"),
+                "Cannot find X",
+                5
+        );
+
+        waitForElementNotVisible(
+                By.id("org.wikipedia:id/page_list_item_title"),
+                "Search results still",
+                5
+
+        );
 
 
     }
-    private WebElement waitForElementPresentByXpath(String xpath, String errorMessage, long timeoutInSeconds)
-    {
-        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
-        wait.withMessage(errorMessage + "\n");
-
-        By by = By.xpath(xpath);
+    private WebElement waitForElement(By by, String error_message, long timeOutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+        wait.withMessage(error_message + "\n");
         return wait.until(
-                ExpectedConditions.presenceOfElementLocated(by)
+                ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    private WebElement waitAndClick(By by, String error_message, long timeOutInSeconds) {
+        WebElement element = waitForElement(by, error_message, 5);
+        element.click();
+        return element;
+
+    }
+
+    private WebElement waitAndSendKeys(By by, String keys, String error_message, long timeOutInSeconds) {
+        WebElement element = waitForElement(by, error_message, 5);
+        element.sendKeys(keys);
+        return element;
+    }
+
+    private boolean waitForElementNotVisible(By by, String error_message, long timeOutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(
+                ExpectedConditions.invisibilityOfElementLocated(by)
         );
     }
-    private WebElement waitforElementPresentByXpath(String xpath, String errorMessage) {
-        return waitForElementPresentByXpath(xpath, errorMessage,5 );
-    }
-
-    private WebElement assertElementHasText (String xpath, String errorMessage, String expectedText,long timeInSeconds )
-    {
-        WebElement element1 = waitForElementPresentByXpath(xpath,errorMessage,timeInSeconds);
-        Assert.assertEquals(expectedText,element1.getText());
-        return element1;
-    }
-
 }
