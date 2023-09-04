01f67535b153be3933d8318ac9bf9d95699c66cc
aamuilman <aamuilman@gmail.com>
2023-09-04 17:33:07 +0300

    Find and close_2

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.net.URL;
+
 public class FirstTest {
 
     private AppiumDriver driver;
